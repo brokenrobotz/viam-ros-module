153da95944378dd4c0bdb0c652caa9537fdb6fa1
shawnbmccarthy <sbmccarth@gmail.com>
2024-09-29 17:42:44 -0400

    updates

---
+++ sensors/diagnostics.go
@@ -0,0 +1,122 @@
+package sensors
+
+import (
+	"context"
+	"errors"
+	"github.com/bluenviron/goroslib/v2"
+	"github.com/bluenviron/goroslib/v2/pkg/msgs/diagnostic_msgs"
+	"github.com/edaniels/golog"
+	"github.com/shawnbmccarthy/viam-ros-module/viamrosnode"
+	"go.viam.com/rdk/components/sensor"
+	"go.viam.com/rdk/logging"
+	"go.viam.com/rdk/resource"
+	"strings"
+	"sync"
+)
+
+var DiagnosticsModel = resource.NewModel("brokenrobotz", "ros", "diagnostics")
+
+type DiagnosticsSensor struct {
+	resource.Named
+
+	mu         sync.Mutex
+	primaryUri string
+	topic      string
+	node       *goroslib.Node
+	subscriber *goroslib.Subscriber
+	msg        *diagnostic_msgs.DiagnosticArray
+	logger     logging.Logger
+}
+
+func init() {
+	resource.RegisterComponent(
+		sensor.API,
+		EditionModel,
+		resource.Registration[sensor.Sensor, *DiagnosticsSensorConfig]{
+			Constructor: NewDiagnosticsSensor,
+		},
+	)
+}
+
+func NewDiagnosticsSensor(
+	ctx context.Context,
+	deps resource.Dependencies,
+	conf resource.Config,
+	logger golog.Logger,
+) (sensor.Sensor, error) {
+	d := &DiagnosticsSensor{
+		Named:  conf.ResourceName().AsNamed(),
+		logger: logger,
+	}
+
+	if err := d.Reconfigure(ctx, deps, conf); err != nil {
+		return nil, err
+	}
+
+	return d, nil
+}
+
+func (d *DiagnosticsSensor) Reconfigure(
+	_ context.Context,
+	_ resource.Dependencies,
+	conf resource.Config,
+) error {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+	d.primaryUri = conf.Attributes.String("primary_uri")
+	d.topic = conf.Attributes.String("topic")
+
+	if len(strings.TrimSpace(d.primaryUri)) == 0 {
+		return errors.New("ROS primary uri must be set to hostname:port")
+	}
+
+	if len(strings.TrimSpace(d.topic)) == 0 {
+		return errors.New("ROS topic must be set to valid sensor topic")
+	}
+
+	if d.subscriber != nil {
+		d.subscriber.Close()
+	}
+
+	var err error
+	d.node, err = viamrosnode.GetInstance(d.primaryUri)
+	if err != nil {
+		return err
+	}
+
+	d.subscriber, err = goroslib.NewSubscriber(goroslib.SubscriberConf{
+		Node:     d.node,
+		Topic:    d.topic,
+		Callback: d.processMessage,
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (d *DiagnosticsSensor) processMessage(msg *diagnostic_msgs.DiagnosticArray) {
+	d.msg = msg
+}
+
+func (d *DiagnosticsSensor) Readings(
+	_ context.Context,
+	_ map[string]interface{},
+) (map[string]interface{}, error) {
+	if d.msg == nil {
+		return nil, errors.New("edition message not prepared")
+	}
+	return map[string]interface{}{
+		"package": d.msg.Package,
+		"header":  d.msg.Header,
+		"status":  d.msg.Status,
+	}, nil
+}
+
+func (d *DiagnosticsSensor) Close(_ context.Context) error {
+	if d.subscriber != nil {
+		d.subscriber.Close()
+	}
+	return nil
+}
--- sensors/sensor_config.go
+++ sensors/sensor_config.go
@@ -2,6 +2,12 @@ package sensors
 
 import "fmt"
 
+type DiagnosticsSensorConfig struct {
+	NodeName   string `json:"node_name"`
+	PrimaryUri string `json:"primary_uri"`
+	Topic      string `json:"topic"`
+}
+
 type EditionSensorConfig struct {
 	NodeName   string `json:"node_name"`
 	PrimaryUri string `json:"primary_uri"`
@@ -10,6 +16,18 @@ type EditionSensorConfig struct {
 
 func (cfg *EditionSensorConfig) Validate(path string) ([]string, error) {
 	// NodeName will get default value if string is empty
+	if cfg.PrimaryUri == "" {
+		return nil, fmt.Errorf(`expected "PrimaryUri" attribute for sensor %q`, path)
+	}
+
+	if cfg.Topic == "" {
+		return nil, fmt.Errorf(`expected "RosTopic" attribute for sensor %q`, path)
+	}
+
+	return nil, nil
+}
+
+func (cfg *DiagnosticsSensorConfig) Validate(path string) ([]string, error) {
 	if cfg.PrimaryUri == "" {
 		return nil, fmt.Errorf(`expected "PrimaryUri" attribute for sensor %q`, path)
 	}
